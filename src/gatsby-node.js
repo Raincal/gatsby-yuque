d5a3edb25db28652ad00b173f95ab8e92188c4d2
Raincal <cyj94228@gmail.com>
2019-08-19 17:14:02 +0800

    :sparkles: process cover image

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,9 +1,36 @@
 const path = require('path')
 const process = require('process')
+const { createRemoteFileNode } = require('gatsby-source-filesystem')
 const cwd = process.cwd()
 
 const getAllArticles = require('./download')
 const { formatDate, formatArray } = require('./utils')
+
+exports.onCreateNode = async (
+	{
+		actions: { createNode },
+		node,
+		createContentDigest,
+		store,
+		cache,
+		reporter,
+	}
+) => {
+	if (node.internal.type === "YuqueDoc" && node.cover) {
+		const fileNode = await createRemoteFileNode({
+			url: node.cover,
+			store,
+			cache,
+			createNode,
+			createNodeId: createContentDigest,
+			reporter,
+		})
+
+		if (fileNode) {
+			node.cover___NODE = fileNode.id
+		}
+	}
+}
 
 exports.sourceNodes = async (context, pluginOptions) => {
 	const {
